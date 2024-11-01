44c3a931e62d0c286e71252e6d95d5412947f119
sawan <abdullahsawan19@gmail.com>
2024-11-01 22:42:59 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+let stars = document.getElementById('stars');
+let moon = document.getElementById('moon');
+let mountains3 = document.getElementById('mountains3');
+let mountains4 = document.getElementById('mountains4');
+let river = document.getElementById('river');
+let boat = document.getElementById('boat');
+let novil = document.querySelector('.novil');
+
+window.onscroll = function(){
+    let value = scrollY;
+    stars.style.left =value + 'px';
+    moon.style.top =value*3 + 'px';
+    mountains3.style.top =value*2 + 'px';
+    mountains4.style.top =value*1.5 + 'px';
+    river.style.top =value + 'px';
+    boat.style.top =value + 'px';
+    boat.style.left =value*3 + 'px';
+    novil.style.fontsize =value + 'px';
+    if( scrollY >= 67){
+        novil.style.fontsize =67 + 'px';
+        novil.style.position ='fixed';
+         if( scrollY >= 478){
+            novil.style.display ='none';
+         }else{
+            novil.style.display ='block';
+         }
+         if(scrollY >= 127){
+            document.querySelector('.main').style.background = 'linear-gradient(#376281,#10001f)'
+         }else{
+            document.querySelector('.main').style.background = 'linear-gradient(#200016,#10001F)'
+
+         }
+    }
+    
+}
